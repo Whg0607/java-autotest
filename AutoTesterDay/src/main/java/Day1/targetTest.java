50156e5c8cf3dda7a29122b5c92ecfce2f7f00ce
Whg0607 <3094273721@qq.com>
2025-03-28 02:36:22 +0800

    Day1:

---
+++ AutoTesterDay/src/main/java/Day1/targetTest.java
@@ -0,0 +1,10 @@
+package Day1;
+
+public class targetTest {
+    public static void main(String[] args) {
+        String targetJob = "自动化测试工程师 ";
+        int targetSalary = 10000;
+        System.out.println("我的岗位目标是 ："+targetJob);
+        System.out.println("期望薪资 ： " + targetSalary+"元/月");
+    }
+}
